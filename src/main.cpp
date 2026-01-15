84efb03f2fc6d0b331bb73eb4ced32e4d91d32f3
JHud <219741840+JamHudson@users.noreply.github.com>
2026-01-15 23:39:26 +0000

    Line of circles

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,20 @@
 #include <bn_keypad.h>
 #include <bn_sprite_ptr.h>
 #include <bn_sprite_items_dot.h>
+#include <bn_log.h>
+#include <bn_vector.h>
 
 int main()
 {
     bn::core::init();
     bn::backdrop::set_color(bn::color(20, 20, 31));
 
-    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(10, 40);
+    bn::vector<bn::sprite_ptr, 10> circles = {};
+    for (int x = -40; x < 40; x += 10)
+    {
+        bn::sprite_ptr newCircle = bn::sprite_items::dot.create_sprite(x, 40);
+        circles.push_back(newCircle);
+    }
 
     while (true)
     {
