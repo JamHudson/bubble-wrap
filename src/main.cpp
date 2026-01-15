8b6b4e51d6d945909b70f58ca6db921b9f065e7a
JHud <219741840+JamHudson@users.noreply.github.com>
2026-01-15 23:17:47 +0000

    Added sprite

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,15 @@
 #include <bn_color.h>
 #include <bn_core.h>
 #include <bn_keypad.h>
+#include <bn_sprite_ptr.h>
+#include <bn_sprite_items_dot.h>
 
 int main()
 {
     bn::core::init();
     bn::backdrop::set_color(bn::color(20, 20, 31));
+
+    bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(10, 40);
 
     while (true)
     {
