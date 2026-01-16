055115655ac64ee05eb62cb7adac86873efc8179
JHud <219741840+JamHudson@users.noreply.github.com>
2026-01-16 00:31:17 +0000

    The circles spin on button input

--- src/main.cpp
+++ src/main.cpp
@@ -24,22 +24,32 @@ int main()
         circles.push_back(newCircle);
     }
 
+    double theta = 0.1;
     while (true)
     {
         if (bn::keypad::a_held())
         {
             bn::backdrop::set_color(bn::color(31, 15, 15));
+            theta = theta - 0.01;
         }
         else if (bn::keypad::b_held())
         {
             bn::backdrop::set_color(bn::color(0, 31, 15));
+            theta = theta + 0.01;
         }
         else
         {
             bn::backdrop::set_color(bn::color(20, 20, 31));
+            theta = 0.0;
         }
         for (int i = 0; i < circles.size(); i++)
         {
+            bn::sprite_ptr circle = circles.at(i);
+            bn::fixed targetX = 15 * bn::sin(theta + (i * 0.1));
+            bn::fixed targetY = 15 * bn::cos(theta + (i * 0.1));
+            circle.set_x(targetX);
+            circle.set_y(targetY);
         }
         bn::core::update();
     }
+}
